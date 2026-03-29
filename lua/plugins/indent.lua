c77eb5de09abf7b857fc2bcfc08f56f615abd58c
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2026-03-29 14:14:51 +0200

    use snacks for indent line

--- lua/plugins/indent.lua
+++
@@ -1,4 +0,0 @@
-return {
-  -- NOTE: Plugins can be added via a link or github org/name. To run setup automatically, use `opts = {}`
-  { 'NMAC427/guess-indent.nvim', opts = {} },
-}
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,12 +3,8 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
-    -- Enable the window module
-    win = {
-      enabled = true,
-      -- Optional: you can set default styles for all snacks windows here
-      style = 'rounded', -- "rounded", "single", "double", "solid", "shadow", "none"
-    },
-    -- Other modules you're likely using
+    -- 1. Enable the ACTUAL UI modules
+    indent = { enabled = true },
+    scroll = { enabled = true }, -- Smooth scrolling
   },
 }
